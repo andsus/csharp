7dcce7647c08f7b50f15eab90ea4b96a6bb1b67e
Andre Susantin <andre.susantin@google.com>
2024-06-21 17:44:31 -0700

    update grade-shcool

--- grade-school/GradeSchool.cs
+++ grade-school/GradeSchool.cs
@@ -2,27 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+class Student : IComparable<Student>
+{
+    public string Name { get; set; }
+    public int Grade { get; set; }
+
+    public int CompareTo(Student? other) =>
+        this.Name.CompareTo(other.Name);
+
+    public Student(string name, int grade)
+    {
+        Name = name;
+        Grade = grade;
+    }
+}
 public class GradeSchool
 {
-    // Grade: [ 2 -> {Bob, Alice}, 5 -> { Bradley, Franklin }]
-    private SortedList<int, SortedSet<String>> students 
-        = new SortedList<int, SortedSet<String>>();
+    private SortedSet<Student> roster;
 
-    // Maintain sorting in Add method
-    public void Add(string student, int grade) {
-        if (!students.ContainsKey(grade)) {
-            students.Add(grade, new SortedSet<string>());
-        }
-        students[grade].Add(student);
+    // Grade: [ 2 -> {Bob, Alice}, 5 -> { Bradley, Franklin }]
+
+    public GradeSchool()
+    {
+        roster = new SortedSet<Student>();
     }
 
-    public IEnumerable<string> Roster() => 
-        students
-            .SelectMany( s => s.Value);
+    // Maintain sorting in Add method
+    public bool Add(string student, int grade) =>
+        roster.Add(new Student(student, grade));
+
+    public IEnumerable<string> Roster() =>
+        roster
+            .OrderBy(s => s.Name)
+            .OrderBy(s => s.Grade)
+            .Select(s => s.Name);
 
     public IEnumerable<string> Grade(int grade) =>
-        students
-        .Where(s => s.Key == grade)
-        .SelectMany(s => s.Value);
-    
+        roster
+        .Where(s => s.Grade == grade)
+        .Select(s => s.Name);
+
 }
